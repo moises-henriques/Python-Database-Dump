a4dcd3c2952bcdf174d0f8691757539a0fb5204e
Moises Henriques <87833867+moises-henriques@users.noreply.github.com>
2022-06-03 20:01:13 -0300

    Server Dump
    Added support to dump all databases from a server using the arguments -s (flag to indicate a server dump process) and -sn (Server name).

--- postgresDump.py
+++ postgresDump.py
@@ -1,42 +1,44 @@
-############################################################################################
-##                    Script to securely dump PostgreSQL Database:                        ##
-##                                                                                        ##
-## Security measures adopted:                                                             ##
-##  - No sensitive information, such as passwords are given to command line;              ##
-##  - No passwords are kept as plain text in the script;                                  ##
-##  - No passwords or other sentive data are kept as plain text in auxiliary files        ##
-##    (pgpass.conf) after the execution.                                                  ##
-##                                                                                        ##
-## Script calling example:                                                                ##
-##  - python postgresDump.py -t -h localhost -db DBName -u postgres -ev postgresDBPass    ##
-##                                                                                        ##
-## Arguments Explanation:                                                                 ##
-##                                                                                        ##
-##  - (-t): Dump table by table of Database (Optional)                                    ##
-##  - (-h): Hostname                                                                      ##
-##  - (-db): Database name                                                                ##
-##  - (-u): Database user                                                                 ##
-##  - (-ev): Name of environment variable set with Database password for the given user   ##
-##                                                                                        ##
-##  Dump File(s) Location(s) and created folders:                                         ##
-##                                                                                        ##
-##  - When the script is executed, it will create and dump files to c:\pgDump. The        ##
-##    configuration file is created under %APPDATA%\postgresql\, if the folder doesn't    ##
-##    exists, it will be created.                                                         ##                                                                                        ##
-##  - The script must run in a python environment provided with all the dependencies      ##
-##    listed in the import section bellow.                                                ##
-##                                                                                        ##
-##  Final observations:                                                                   ##
-##                                                                                        ##
-##  - The configuration file is overriden after the code execution, so no sensitive data  ##
-##    will be stored in plain text after the dump process.                                ##
-##                                                                                        ##
-##  Any advices or suggestions:                                                           ##
-##                                                                                        ##
-##  - moises.morais.henriques@gmail.com                                                   ##
-##  - https://github.com/moises-henriques                                                 ##
-##                                                                                        ##
-############################################################################################
+#####################################################################################################
+##                    Script to securely dump PostgreSQL Database:                                 ##
+##                                                                                                 ##
+## Security measures adopted:                                                                      ##
+##  - No sensitive information, such as passwords are given to command line;                       ##
+##  - No passwords are kept as plain text in the script;                                           ##
+##  - No passwords or other sentive data are kept as plain text in auxiliary files                 ##
+##    (pgpass.conf) after the execution.                                                           ##
+##                                                                                                 ##
+## Script calling example:                                                                         ##
+##  - python postgresDump.py -t -h localhost -db DBName -u postgres -ev postgresDBPass             ##
+##  - python postgresDump.py -h localhost -p port -s -sn serverName -u admin -ev postgresDBPass    ##                                                                                    ##
+## Arguments Explanation:                                                                          ##
+##                                                                                                 ##
+##  - (-t): Dump table by table of Database (Optional)                                             ##
+##  - (-h): Hostname                                                                               ##
+##  - (-db): Database name                                                                         ##
+##  - (-u): Database user                                                                          ##
+##  - (-ev): Name of environment variable set with Database password for the given user            ##
+##  - (-s): Dump all databases found on the server                                                 ##
+##  - (-sn): Server Name                                                                           ##
+##                                                                                                 ##
+##  Dump File(s) Location(s) and created folders:                                                  ##
+##                                                                                                 ##
+##  - When the script is executed, it will create and dump files to c:\pgDump. The                 ##
+##    configuration file is created under %APPDATA%\postgresql\, if the folder doesn't             ##
+##    exists, it will be created.                                                                  ##                                                                                        ##
+##  - The script must run in a python environment provided with all the dependencies               ##
+##    listed in the import section bellow.                                                         ##
+##                                                                                                 ##
+##  Final observations:                                                                            ##
+##                                                                                                 ##
+##  - The configuration file is overriden after the code execution, so no sensitive data           ##
+##    will be stored in plain text after the dump process.                                         ##
+##                                                                                                 ##
+##  Any advices or suggestions:                                                                    ##
+##                                                                                                 ##
+##  - moises.morais.henriques@gmail.com                                                            ##
+##  - https://github.com/moises-henriques                                                          ##
+##                                                                                                 ##
+#####################################################################################################
 
 
 ## Imports ##
@@ -49,6 +51,11 @@ import os
 ## Global Flags ##
 compressFlag = False
 environPassword = 'postgresDBPass'
+host = 'localhost'
+database="DBName"
+port="5432"
+serverDump = False
+serverName = 'ServerName'
 
 ## Connect to Postgres DB
 def DBConnect(**kwargs):
@@ -83,7 +90,7 @@ def DBConnect(**kwargs):
         user=user,
         password= password)
     except (Exception, psycopg2.DatabaseError) as error:
-        return error, None
+        raise psycopg2.OperationalError(error.args[0])
     else:
         print('A connection was successfully established with server!')
         cursor = conn.cursor()
@@ -152,32 +159,47 @@ def clearPgpass():
 
     
 ## List all table names ##
-def backupDB(DBName,user,password,**kwargs):
+def backupDB(host,port,user,password,**kwargs):
+    if 'DBName' in kwargs:
+        DBName = kwargs.get('DBName')
+    elif 'serverName' in kwargs:
+        serverName = kwargs.get('serverName')
+        DBName='*'
+    else:
+        raise RuntimeError('ERROR: Database Name or Server Name must be provided to backupDB function.')
     if 'mode' in kwargs:
         mode = kwargs.get('mode')
         if mode == 'byTable':
             ## Connect ##
-            conn, cursor = DBConnect(user= user,database=DBName,password=password)
+            conn, cursor = DBConnect(host=host,user= user,database=DBName,password=password)
             ## Get table list in DB ##
             tableList = queryTableList(conn, cursor)
             conn.close()
     else:
         mode="all"
     
+    
+    
     try:
-        if setPGPass(DBName,user,password):
+        if setPGPass(DBName,user,password,host=host):
             if not os.path.isdir('c:\pgDump\\'):
                 os.mkdir('c:\pgDump\\')
-            if mode == 'all':
-                pg_dump = '"pg_dump -U '+user+' -F t '+DBName+' > c:\pgDump\\'+DBName+'.tar"'
-                os.system('"cmd.exe"' and pg_dump)
+            global serverDump
 
-            elif mode ==  'byTable':
-                for idx in tableList.index:
-                    pg_dump = '"pg_dump -U '+user+' -F t --table public.'+tableList.iloc[idx][0]+' '+DBName+' > c:\pgDump\\'+DBName+'_tb_'+tableList.iloc[idx][0]+'.tar"'
+            if not serverDump:
+                if mode == 'all':
+                    pg_dump = '"pg_dump -h '+host+' -p '+port+' -U '+user+' -F t '+DBName+' > c:\pgDump\\'+DBName+'.tar"'
                     os.system('"cmd.exe"' and pg_dump)
+
+                elif mode ==  'byTable':
+                    for idx in tableList.index:
+                        pg_dump = '"pg_dump -h '+host+' -p '+port+' -U '+user+' -F t --table public.'+tableList.iloc[idx][0]+' '+DBName+' > c:\pgDump\\'+DBName+'_tb_'+tableList.iloc[idx][0]+'.tar"'
+                        os.system('"cmd.exe"' and pg_dump)
+                else:
+                    raise ValueError('ERROR: Invalid mode provided in backupDB function.')
             else:
-                raise ValueError('ERROR: Invalid mode provided in backupDB function.')
+                pg_dumpall = '"pg_dumpall -h '+host+' -p '+port+' -U '+user+' > c:\pgDump\\'+serverName+'.dump"'
+                os.system('"cmd.exe"' and pg_dumpall)
         else:
             raise RuntimeError('ERROR: We could not Set/Create the pgpass.conf, review the code.')
         if clearPgpass():
@@ -190,6 +212,23 @@ def backupDB(DBName,user,password,**kwargs):
 
 ## Get initial arguments and set flags ##
 def getArguments():
+    # Server dump
+    global serverDump
+    global serverName
+    if '-s' in sys.argv:
+        serverDump = True
+        if '-sn' in sys.argv:
+            serverName = None
+            idx = 0
+            for entry in sys.argv:
+                if entry == '-sn':
+                    serverName = sys.argv[idx+1]
+                    break
+                else:
+                    idx += 1    
+    else:
+        serverDump = False
+
     # mode
     if '-t' in sys.argv:
         mode='byTable'
@@ -197,6 +236,7 @@ def getArguments():
         mode = 'all'
 
     # host
+    global host
     host = None
     if '-h' in sys.argv:
         idx = 0
@@ -208,7 +248,7 @@ def getArguments():
                 idx += 1
                 
     # Port
-    port = None
+    global port
     if '-p' in sys.argv:
         idx = 0
         for entry in sys.argv:
@@ -217,10 +257,9 @@ def getArguments():
                 break
             else:
                 idx += 1
-    if not port:
-        port = 3306
 
     # Data Base
+    global DBName
     DBName = None
     if '-db' in sys.argv:
         idx = 0
@@ -253,23 +292,41 @@ def getArguments():
             else:
                 idx += 1
 
-    if not (DBName or host or user):
-        raise ValueError('Database Name and/or host arguments missing.')
+    if serverDump:
+        if not (serverName or host or user):
+            raise ValueError('Server Name and/or host and or user arguments missing.')
+        else:
+            return host, port, serverName, user, environPassword 
     else:
-        return mode, host, port, DBName, user, environPassword
+        if not (DBName or host or user):
+            raise ValueError('Database Name and/or host arguments missing.')
+        else:
+            return mode, host, port, DBName, user, environPassword
 
 ### Main ###
 if __name__ == '__main__':
     executionTime = datetime.now()
     ## get arguments ##
-    mode, host, port, DBName, user, environPassword = getArguments()
-
-    ## Run Backup ##
-    if backupDB(DBName=DBName,user=user,password=os.getenv(environPassword),mode = mode):
-        print('Backup finished, you can find your files on c:\pgDump directory.')
-        executionTime = datetime.now()-executionTime
-        print('Execution time: {}'.format(executionTime))
+    if '-s' in sys.argv:
+        host, port, serverName, user, environPassword = getArguments()
     else:
-        print('The backup could not be done.')
-        executionTime = datetime.now()-executionTime
-        print('Execution time: {}'.format(executionTime))
+        mode, host, port, DBName, user, environPassword = getArguments()
+
+    if serverDump:
+        if backupDB(serverName=serverName,host=host,port=port,user=user,password=os.getenv(environPassword)):
+            print('Backup finished, you can find your files on c:\pgDump directory.')
+            executionTime = datetime.now()-executionTime
+            print('Execution time: {}'.format(executionTime))
+        else:
+            print('The backup could not be done.')
+            executionTime = datetime.now()-executionTime
+            print('Execution time: {}'.format(executionTime))
+    else:
+        if backupDB(DBName=DBName,host=host,port=port,user=user,password=os.getenv(environPassword),mode = mode):
+            print('Backup finished, you can find your files on c:\pgDump directory.')
+            executionTime = datetime.now()-executionTime
+            print('Execution time: {}'.format(executionTime))
+        else:
+            print('The backup could not be done.')
+            executionTime = datetime.now()-executionTime
+            print('Execution time: {}'.format(executionTime))
